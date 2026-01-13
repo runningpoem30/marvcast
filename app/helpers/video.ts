5ab375445580bf65dd1a4da1a269c9a3fe1bceb2
runningpoem30 <aryaanandpathak30@gmail.com>
2026-01-14 04:10:46 +0530

    blob buffer

--- app/helpers/video.ts
+++ app/helpers/video.ts
@@ -40,5 +40,17 @@ export async function trimVideo(
 
   const data = await ffmpeg.readFile("output.webm");
 
-  return new Blob([data], { type: "video/webm" });
+
+  const uint8 =
+  typeof data === "string"
+    ? new TextEncoder().encode(data)
+    : data;
+
+// Step 2: create a NEW ArrayBuffer (this is the key)
+const arrayBuffer = new ArrayBuffer(uint8.byteLength);
+const view = new Uint8Array(arrayBuffer);
+view.set(uint8);
+
+// Step 3: Blob from safe ArrayBuffer
+return new Blob([arrayBuffer], { type: "video/webm" });
 }
